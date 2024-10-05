35ef060aa5573fe302be195a39fa8bfe1fa1b08a
Shouklier <tawfikcraft@gmail.com>
2024-10-05 16:21:23 +0300

    Added functionality, validation and local file saving (finalised program)

--- CLI-To-Do-List.py
+++ CLI-To-Do-List.py
@@ -13,6 +13,18 @@ def displayCommandList():
     print("del <List_Item_Index> : Remove list item of index <List_Item_Index>")
     print("qit : Quits program and saves to loaded file\n")
 
+validCommands = set(["hlp", "add", "del", "qit"])
+
+def addToList(listItem):
+    listofitems.append(listItem)
+def delFromList(index):
+    listofitems.pop(int(index) - 1)
+def willsave():
+    print()
+    willSave = input("Do you want to save in original file? (yes/no)")
+    
+    return willSave == "yes"
+
 if filesInDirectory:
     print("Here is a list of the text files in this directory:\n")
     count = 1
@@ -36,17 +48,47 @@ if filesInDirectory:
 else:
     print("No text files in directory.\n Fresh start:)")
 
-while True:
-    if not listofitems:
-        print("No items in list")
-    else:
-        print("To-Do List:\n")
-        for index in range(len(listofitems)):
-            print(str(index + 1) + ". " + listofitems[index])
+looping = True
+printing = True
 
-    print("\nEnter a command or 'hlp' to get a list of commands")
+while looping:
+    if printing:
+        if not listofitems:
+            print("No items in list")
+        else:
+            print("To-Do List:\n")
+            for index in range(len(listofitems)):
+                print(str(index + 1) + ". " + listofitems[index])
+
+        print("\nEnter a command or 'hlp' to get a list of commands.")
+    printing = True
     command = input()
-    
-    while command == "hlp":
+
+    if len(command) < 3 or command[:3] not in validCommands:
+        print("\nInvalid Command.\nEnter a command or 'hlp' to get a list of commands.\n")
+        printing = False
+
+    if command[:3] == "hlp":
         displayCommandList()
-        command = input()
+        printing = False
+    
+    elif command[:3] == "add":
+        addToList(command[4:])
+    
+    elif command[:3] == "del":
+        delFromList(command[4:])
+    
+    elif command[:3] == "qit" and input("\nAre you sure you want to leave? (yes/no)\n") == "yes":
+        looping = False
+        if willsave():
+            with open(filetouse, 'w') as file:
+                file.write("\n".join(listofitems))
+        else:
+            pass
+
+print("\nThanks for using my program and have a great day 😀")
+
+    
+    
+
+    
